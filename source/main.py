0c8eabc274ced827651df39133a248d10c7e011c
Michalina Śledzikowska <236669@edu.p.lodz.pl>
2022-04-30 00:55:53 +0200

    Create user interface and example file for sending

--- source/main.py
+++ source/main.py
@@ -1,37 +1,96 @@
-from bitarray import bitarray
-
-import huffman as h
+from genericpath import exists
+from rich.markdown import Markdown
+from rich.console import Console
+from rich.prompt import Prompt
+import socket
+import time
 import tcp_conection as con
+import os
 
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+console = Console()
+waiter_txt = "Wciśnij dowolny klawisz, aby kontynuować..."
 
 def main():
-    message_str = "Założyć gęślą jaźń"
-    print(message_str)
+    message = "Zażółć gęślą jaźń"
+    path = "plik.txt"
+    user_input = ''
+    host_address = "127.0.0.1"
+    port = 61111
+    while user_input != 'q':
+        cls()
+        console.print("[bold][cyan]Wiadomość:[/][/]\n", message)
+        console.print(f"[bold][cyan]Adres:[/][/] \n {host_address} : {port}\n")
+
+        console.print(('''Podaj, czy chcesz:
+        (1) Ustawić wiadomość
+        (2) Ustawić adres hosta
+        (3) Ustawić port
+        (4) Nadać wiadomość
+        (5) Odebrać wiadomość
+        [dim]\[q - wyjście][/]'''))
+        user_input = input("> ")
+        print()
+        match user_input:
+            case '1': message, path = get_message(path) or (message, path)
+            case '2': host_address = input("Podaj adres: ")
+            case '3': port = int(input("Podaj port: "))
+            case '4': send_message(message, host_address, port)
+            case '5': receive_message(host_address, port)
+            case 'q': break
+            case _: continue
+
+
+def get_message(path) -> (str, str):
+    console.print('''Wczytaj wiadomość
+    (1) Z konsoli
+    (2) Z pliku''')
+    match input("> "):
+        case '1':
+            message = input("Podaj treść wiadomości: ")
+            if message != "": 
+                return (message, path)
+        case '2':
+            path = Prompt.ask(f"Podaj ścieżkę", default=path)
+            if exists(path):
+                with open(path) as file:
+                    return (file.read(), path)
+            else:
+                input(f"Nie znaleziono pliku. {waiter_txt}")
+
+
+def send_message(message_str, host, port):
     message = bytes(message_str, 'utf-8')
-    frequency_list = h.calculate_frequency(message)
-    root = h.create_huffman_tree(frequency_list)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        for i in range(60):
+            try:
+                s.connect((host, port))
+                con.send_data(s, message)
+                input(f"Wysłano! {waiter_txt}")
+                break
+            except ConnectionRefusedError:
+                console.print(f"{i + 1} Odbiorca nie nawiązał połączenia")
+                time.sleep(1)
 
-    dictionary = h.create_huffman_dictionary(root)
-    compressed = h.compress(message, dictionary)
 
-    print(frequency_list)
-    print(dictionary)
-
-    test = bitarray()
-    test.frombytes(compressed)
-    print(test)
-
-    decompressed = h.decompress(compressed, len(message), root)
-    print(str(decompressed, 'utf-8'))
-
-    header = con.create_connection_header(6500, 2137, frequency_list, "ziemniaki.txt")
-    message_type, message_length, content_length, filename, frequency_table = con.read_data_from_header(header)
-    print(message_type)
-    print(message_length)
-    print(content_length)
-    print(filename)
-    print(frequency_table)
-
+def receive_message(host, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            console.print(f"Connected by {addr}")
+            message_type, message = con.receive_data(conn)
+            print()
+            console.print(str(message, 'utf-8'))
+            print()
+    path = Prompt.ask("Gdzie zapisać plik? [dim]\[wciśnij Enter by pominąć][/]")
+    if(path != ''):
+        with open(path, 'w') as file:
+            file.write(message.decode("utf-8"))
+            input(f"Zapisano plik! {waiter_txt}")
 
 if __name__ == "__main__":
     main()
